f4b10d99a35da51433afdedc6bafecae82d57b88
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2019-09-24 14:18:26 +0200

    Typescript mode
    Signed-off-by: Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -590,6 +590,21 @@
   :ensure t
   )
 
+(use-package typescript-mode
+  :ensure t)
+
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+
+;; MMMFF
+
+(use-package maude-mode
+  :ensure t)
+
 (provide 'init)
 
 ;;; init.el ends here
